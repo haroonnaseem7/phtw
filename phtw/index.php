e6ce2b79ac1719b615ffda98995b05d50958b620
haroonnaseem7 <129225879+haroonnaseem7@users.noreply.github.com>
2023-11-03 14:49:13 +0530

    Update index.php

--- phtw/index.php
+++ phtw/index.php
@@ -1,32 +1,5 @@
-<?php
-/**
- * Campaign: Pizzahut TW
- * Created: 2023-10-13 08:37:37 UTC
- */
+<html>
 
-require 'leadcloak-1cf8i7e53f59.php';
+<meta http-equiv="refresh" content="URL=https://www.pizzahut.com.tw" />
 
-// ---------------------------------------------------
-// Configuration
-
-// Set this to false if application is properly installed.
-$enableDebugging = false;
-
-// Set this to false if you won't want to log error messages
-$enableLogging = true;
-
-if ($enableDebugging) {
-	isApplicationReadyToRun();
-}
-
-$data = httpRequestMakePayload($campaignId, $campaignSignature);
-
-$response = httpRequestExec($data);
-
-$handler = httpHandleResponse($response, $enableLogging);
-
-if ($handler) {
-	exit();
-}
-
-?>
+<html>
